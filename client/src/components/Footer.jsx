60f8a518491b3091044774db8a1a3392c6ad0652
MarcArcher <marcarcher1999@gmail.com>
2022-06-13 20:38:04 +0200

    resolve img dont load

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
             </div>
             <div className="flex justify-center items-center flex-col mt-5">
                 <p className="text-white text-sm text-center">Come join us</p>
-                <p className="text-white text-sm text-center">info@blockcoin.com</p>
+                <p className="text-white text-sm text-center">info@blockcoin.tech</p>
             </div>
             <div className="sm:w-[90%] w-full h-[0.25px] bg-gray-400 mt-5"/>
             <div className="sm:w-[90%] w-full flex justify-between items-center mt-3">
